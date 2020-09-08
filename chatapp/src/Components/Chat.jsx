e6b853e5c5ea71b0ba15cf2c464e465b3f455b85
Northcoders <northcoders@Northcoderss-MacBook-Pro.local>
2020-09-08 13:03:01 +0100

    Chat component begin on functionality of passing logged in user from Login component

--- chatapp/src/Components/Chat.jsx
+++ chatapp/src/Components/Chat.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
 class Chat extends Component {
+  state = {
+    username: this.props.location.state
+      ? this.props.location.state.username
+      : null,
+    url: this.props.location.state ? this.props.location.state.url : null,
+  };
   render() {
-    return <h1>CHAT</h1>;
+    const { username, url } = this.state;
+    //https://chaewonkong.github.io/posts/socket-io.html#Sending-and-Receiving-Messages
+    if (!username || !url) {
+      ///return error page
+      console.log("In the error render...");
+    }
+
+    return <section>chat goes here....</section>;
   }
 }
 
